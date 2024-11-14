b1a9b130dc8c9b1a36d020c498e51ce855b6fc99
pranRV <pranava.rchs@gmail.com>
2024-11-14 12:20:55 +0530

    SAMPLE MAVEN JAVA

---
+++ src/main/java/com/app/demo/JavaMavenExample/App.java
@@ -0,0 +1,11 @@
+package com.app.demo.JavaMavenExample;
+
+/**
+ * Hello world! clean install test build goals target .jar
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Pranava!");
+        System.out.println("22BD1A053X");
+    }
+}
