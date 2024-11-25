a839bedb2c6083050d8a054ca24b25ca963faf33
pranRV <147342758+pranRV@users.noreply.github.com>
2024-11-26 00:26:09 +0530

    Update App.java

--- src/main/java/com/app/demo/JavaMavenExample/App.java
+++ src/main/java/com/app/demo/JavaMavenExample/App.java
@@ -5,6 +5,6 @@ package com.app.demo.JavaMavenExample;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hellooo!");
+        System.out.println("Hellooo i made a change!");
     }
 }
